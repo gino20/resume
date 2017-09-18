fd46687705af2fc04bf65cb3250422b2c85bb673
gino20 <gino20.hong@gmail.com>
2017-09-18 09:51:27 +0800

    refactor: update dist files

--- rollup.config.js
+++ rollup.config.js
@@ -1,28 +1,26 @@
 import typescript from 'rollup-plugin-typescript';
 import rollupNodeResolve from 'rollup-plugin-node-resolve';
 import rollupCommonjs from 'rollup-plugin-commonjs';
-import uglify from 'rollup-plugin-uglify';
+import uglify from 'rollup-plugin-uglify-es';
 import livereload from 'rollup-plugin-livereload'
 import serve from 'rollup-plugin-serve'
 import nodeGlobals from 'rollup-plugin-node-globals'
 import vue from 'rollup-plugin-vue';
-import postcss from 'rollup-plugin-postcss';
-import alias from 'rollup-plugin-alias'
 import svg from 'rollup-plugin-svg';
 
-const isProd = process.env.NODE_ENV === `production`
-const isDev = process.env.NODE_ENV === `development`
+const isProd = process.env.NODE_ENV === 'production'
+const isDev = process.env.NODE_ENV === 'development'
 
 let config = {
   input: 'src/main.ts',
   output: {
-    file: 'dest/bundle.js',
+    file: 'dist/bundle.js',
     format: 'iife'
   },
   plugins: [
     typescript(),
     vue({
-      css: 'dest/bundle.css'
+      css: 'dist/bundle.css'
     }),
     rollupNodeResolve({
       jsnext: true,
@@ -31,7 +29,6 @@ let config = {
     }),
     rollupCommonjs(),
     nodeGlobals(),
-    postcss(),
     svg()
   ]
 }
