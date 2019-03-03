212e4bfbdbc0e6fb19a0c71aaeb7d70f4c2c839f
gino20 <gino20.hong@gmail.com>
2019-03-03 17:50:58 +0800

    fix: udpate vue config

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production' ? '/resume/' : '/',
+  publicPath: './',
   chainWebpack: config => {
     const svgRule = config.module.rule('svg')
 
